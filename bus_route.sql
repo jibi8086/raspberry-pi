6583e39a16c5eae4df4c034c371173aeda50c0d5
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2018-02-28 00:57:29 +0530

    Update bus_route.sql

--- bus_route.sql
+++ bus_route.sql
@@ -1,9 +1,9 @@
-select Buses.bus_name,s1.stop_name as from_stop_name,t1.departure_time,s2.stop_name as to_stop_name,t2.arrival_time
-   from buses
-inner join  route  t1 on t1.bus_id = buses.id
-inner join Stop s1 on s1.id = t1.stop_id
-inner join route t2 on t2.bus_id = buses.id
-inner join Stop s2 on s2.id = t2.stop_id
-where Buses.bus_name in (select bus_name from Buses)
-and s1.stop_name = 'aluva'
-   and s2.stop_name = 'angamaly'
+select Buses.bus_name,s1.stop_name as from_stop_name,t1.departure_time,s2.stop_name
+   as to_stop_name,t2.arrival_time 
+   from Buses 
+   inner join route t1 on t1.bus_id = Buses.id 
+   inner join Stop s1 on s1.id = t1.stop_id 
+   inner join route t2 on t2.bus_id = Buses.id
+   inner join Stop s2 on s2.id = t2.stop_id
+   where Buses.bus_name in (select bus_name from Buses) 
+   and s1.stop_name = 'aluva' and s2.stop_name = 'angamaly'
