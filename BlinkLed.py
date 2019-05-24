37460a78e986ad9c48673a5501c06adb0cf7c877
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2019-05-24 12:49:28 -0700

    Update BlinkLed.py

--- BlinkLed.py
+++ BlinkLed.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
-GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 while True: # Run forever
