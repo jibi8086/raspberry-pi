4acc98b62e472ff23e4f59314a2db48d27787698
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2019-05-31 12:23:56 -0700

    Update arduinoWithPi.py

--- arduinoWithPi.py
+++ arduinoWithPi.py
@@ -1,1 +1,8 @@
-
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.IN)
+while True:
+	input = GPIO.input(21)
+	if(input==1):
+		print("Pressure sensor Medium squeeze")
